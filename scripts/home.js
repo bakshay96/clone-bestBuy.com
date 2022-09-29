0e22886838bbfab832bf99b64d27812b0f10f1f5
Avinash Patel <avinashpatel9755@gmail.com>
2022-09-30 00:27:08 +0530

    Adding

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,11 @@
+/*********** IMPORT NAVBAR ***********/
+
+import { navbar } from "../components/navbar.js";
+
+let navbar_div = document.getElementById('navbar');
+navbar_div.innerHTML = navbar();
+
+
 /********** When the user clicks on the button, 
 toggle between hiding and showing the dropdown content **********/
 
@@ -7,17 +15,3 @@ const drop_menu = document.querySelector('.dropdown');
 drop_btn.addEventListener("click", () => {
     drop_menu.classList.toggle("hide");
 });
-
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
