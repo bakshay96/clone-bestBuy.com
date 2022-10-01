e2a37e34cc17eb5bd36115ef9ed26f48019acf2f
bakshay96 <akshaymali.edu@gmail.com>
2022-10-02 04:21:46 +0530

    login/signup testing completed

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,8 +1,9 @@
 
-async function register(){
-
+async function register(event){
+   
+    event.preventDefault();
     //collect all all the data
-  
+  let form=document.querySelector('form');
 
     let register_data={
         fname :  document.getElementById('fname').value,
@@ -16,20 +17,31 @@ async function register(){
     };
    let lname =  document.getElementById('lname').value;
    let cpassword= document.getElementById('confirm-password').value;
-
-    if(register_data.password==cpassword)
+   let data;
+    if(register_data.password==cpassword && form.fname!=""&& form.email!=""&& form.mobile!="")
     {
-        let res= await fetch(`https://masai-api-mocker.herokuapp.com/auth/register`,
+       try
+       { 
+            let res= await fetch(`https://masai-api-mocker.herokuapp.com/auth/register`,
+            {
+                method:`POST`,
+                body :JSON.stringify(register_data),
+                headers:{
+                    'Content-Type':`application/json`,
+                },
+            });
+             data= await  res.json();
+            console.log("server message:-",data.message);
+            console.log("erro msg",data.error)
+        }
+        catch(err)
         {
-            method:`POST`,
-            body :JSON.stringify(register_data),
-            headers:{
-                'Content-Type':`application/json`,
-            },
-        });
-        let data= await  res.json();
-        console.log("server message:-",data.message);
-        console.log("erro msg",data.error)
+            console.log("DATA",err);
+            alert("Server Error")
+            window.location.href="./login.html"
+
+        }
+       
         if(data.error===false)
         {
             alert(data.message)
@@ -37,9 +49,10 @@ async function register(){
         }
         else
         {
+
             alert(data.message)
-            document.getElementById('name').value=null;
-            document.getElementById('password').value=null;
+            form.username.value=null;
+            form.fname=null;
             document.getElementById('username').value=null;
             
         }
@@ -51,8 +64,61 @@ async function register(){
     }
 
     console.log("Registered_Data",register_data)
+    alert("Account created sucessfully")
 
    // base url-https://masai-api-mocker.herokuapp.com/
 
    
 };
+
+// // ------------------------------//local data validation-----------------------
+
+// function register()
+// {
+//     let form = document.querySelector("form");
+//     let signupLS = JSON.parse(localStorage.getItem("signup"))|| [];
+
+//     form.addEventListener("submit",function(event){
+//         event.preventDefault();
+//         let obj={
+//             fname :  document.getElementById('fname').value,
+//             lname :  document.getElementById('lname').value,
+//             email : document.getElementById('email').value,
+//             password: document.getElementById('password').value,
+//             mobile: document.getElementById('mobile').value,
+//             // cpassword: document.getElementById('confirm-password').value,
+        
+        
+        
+
+//         };
+//         // let obj2 ={
+//         //     name:form.fname.value,
+//         //     email:form.email.value,
+//         //     password:form.password.value,
+//         // }
+   
+//     // if(obj.cpassword!=obj.password)
+//     // {
+//     //     alert("Enter same password !")
+//     //     document.getElementById('confirm-password').value="";
+       
+      
+//     // }
+//      if(obj.fname == "" || obj.email=="" || obj.password == "" || obj.mobile==""){
+//             alert("Field Empty");
+//             return;
+//     }
+//     else{
+//             signupLS.push(obj);
+//             localStorage.setItem("signup",JSON.stringify(signupLS));
+//             alert("sinup sucessfull..!")
+            
+//             window.location.href="./login.html";
+        
+      
+        
+//         }
+        
+//     })
+// }
