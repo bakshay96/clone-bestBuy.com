5c45b715a42bf77832ef58a9c655657194e9114d
Avinash Patel <avinashpatel9755@gmail.com>
2022-10-03 15:34:50 +0530

    Adding

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,3 @@
-
 //     let cart = JSON.parse(localStorage.getItem('cart'));
 //     let container = document.getElementById("cart-product");
 //     let product = document.getElementById("cart-product");
@@ -33,52 +32,49 @@ let product = document.getElementById("cart-product");
 let Subtotal = 0;
 //get the items from local storage
 let container = document.getElementById("cart-product");
-let cart= JSON.parse( localStorage.getItem("cart"))||[];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 console.log(cart);
 
-function display(cart)
-{
-    container.innerHTML=null;
-    cart.forEach((el,index)=>{
-    let div = document.createElement("div");
-    div.setAttribute("class","product");
-    let img = document.createElement("img");
-    img.src=el.image;
-    let title = document.createElement("h3");
-    title.innerText=el.name;
-    let price = document.createElement("p");
-    price.innerText=el.price;
-    let removeItem=document.createElement("button");
-    removeItem.innerText="Remove Item"
-    removeItem.addEventListener("click",function()
-    {
-        deleteItem(el);
-    })
+function display(cart) {
+    container.innerHTML = null;
+    cart.forEach((el, index) => {
+        let div = document.createElement("div");
+        div.setAttribute("class", "product");
+        let img = document.createElement("img");
+        img.src = el.image;
+        let title = document.createElement("h3");
+        title.innerText = el.name;
+        let price = document.createElement("p");
+        price.innerText = el.price;
+        let removeItem = document.createElement("button");
+        removeItem.innerText = "Remove Item";
+        removeItem.setAttribute('id', 'btnbtn');
+        removeItem.addEventListener("click", function() {
+            deleteItem(el);
+        })
 
-    Subtotal+=Number(el.priceb)
-    console.log(Subtotal)
-    div.append(img,title,price,removeItem);
-    container.append(div);
+        Subtotal += Number(el.priceb)
+        console.log(Subtotal)
+        div.append(img, title, price, removeItem);
+        container.append(div);
     })
 
     // console.log(Subtotal)
     let subtotal = document.getElementById("subtotal")
-    subtotal.innerText=`$. ${Subtotal}`;
-    localStorage.setItem("subtotal",JSON.stringify(Subtotal))
+    subtotal.innerText = `$. ${Subtotal}`;
+    localStorage.setItem("subtotal", JSON.stringify(Subtotal))
 }
 display(cart);
 
-function deleteItem(index)
-{
-    let deleteData=cart.filter(function(e,i)
-    {
-        return e!=index;
+function deleteItem(index) {
+    let deleteData = cart.filter(function(e, i) {
+        return e != index;
     });
 
-    localStorage.setItem("cart",JSON.stringify(deleteData))
-    cart= JSON.parse( localStorage.getItem("cart"))||[];
+    localStorage.setItem("cart", JSON.stringify(deleteData))
+    cart = JSON.parse(localStorage.getItem("cart")) || [];
     display(cart);
-    window.location. reload();
-    
+    window.location.reload();
+
 }
